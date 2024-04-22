006d274179cbaf4c436c74e3414a093a12de1454
ayomideajayi <ayomideseaz@gmail.com>
2024-04-22 12:18:29 +0100

    changed wallet page design

---
+++ lib/features/wallet/presentation/screens/wallet_tab.dart
@@ -0,0 +1,181 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:get/get.dart';
+import 'package:intl/intl.dart';
+import 'package:tl_consultant/app/presentation/theme/colors.dart';
+import 'package:tl_consultant/app/presentation/theme/fonts.dart';
+import 'package:tl_consultant/app/presentation/widgets/buttons.dart';
+import 'package:tl_consultant/core/utils/helpers/svg_elements.dart';
+import 'package:tl_consultant/features/wallet/presentation/controllers/earnings_controller.dart';
+import 'package:tl_consultant/features/wallet/presentation/widgets/transaction_item.dart';
+
+class WalletTab extends StatefulWidget {
+  const WalletTab({super.key});
+
+  @override
+  State<WalletTab> createState() => _WalletTabState();
+}
+
+class _WalletTabState extends State<WalletTab> {
+  final earningsController = Get.put(EarningsController());
+  NumberFormat? formatCurrency;
+
+  getEarnings() async {
+    earningsController.getEarningsInfo();
+  }
+
+  @override
+  void initState() {
+    formatCurrency = NumberFormat.currency(locale: "en_NG", symbol: '₦');
+
+    getEarnings();
+
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+        padding: EdgeInsets.all(16),
+        child: Column(crossAxisAlignment: CrossAxisAlignment.center, children: [
+          Container(
+            decoration: BoxDecoration(
+              color: ColorPalette.green[200], // Dark tint overlay
+              borderRadius: BorderRadius.circular(16.0),
+            ),
+            child: ClipRRect(
+                borderRadius: BorderRadius.circular(16.0),
+                child: Stack(
+                  clipBehavior: Clip.none,
+                  children: [
+                    SvgPicture.asset(SvgElements.svgbalanceBgImg),
+                    Container(
+                        alignment: Alignment.center,
+                        padding: EdgeInsets.only(top: 24),
+                        child: Column(children: [
+                          const Text(
+                            "Available Balance",
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: ColorPalette.black),
+                          ),
+                          SizedBox(height: 4),
+                          Wrap(
+                              spacing: 25,
+                              crossAxisAlignment: WrapCrossAlignment.center,
+                              children: [
+                                GestureDetector(
+                                    child: Icon(Icons.refresh_outlined,
+                                        color:
+                                            ColorPalette.black.withOpacity(.6)),
+                                    onTap: () async => await getEarnings()),
+                                Text(
+                                    formatCurrency!.format(
+                                        earningsController.balance.value),
+                                    style: TextStyle(
+                                      color: ColorPalette.green[800],
+                                      fontSize: 40,
+                                      fontWeight: FontWeight.bold,
+                                    )),
+                                Icon(Icons.visibility_outlined,
+                                    color: ColorPalette.black.withOpacity(.6))
+                              ]),
+                          SizedBox(height: 12.5),
+                          RichText(
+                            text: TextSpan(
+                                text: "Total Balance: ",
+                                style: TextStyle(
+                                    fontSize: 12,
+                                    color: ColorPalette.black,
+                                    fontFamily: AppFonts.josefinSansRegular),
+                                children: [
+                                  TextSpan(
+                                    text: formatCurrency!.format(
+                                        earningsController.balance.value +
+                                            earningsController
+                                                .pendingClearance.value),
+                                    style: TextStyle(
+                                        color: ColorPalette.green[800],
+                                        fontSize: AppFonts.defaultSize),
+                                  ),
+                                ]),
+                          ),
+                        ]))
+                  ],
+                )),
+          ),
+          const SizedBox(height: 20),
+          Padding(
+            padding: const EdgeInsets.only(left: 48, right: 48),
+            child: CustomButton(
+              onPressed: () {},
+              child: const Center(
+                child:
+                    Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                  Text(
+                    'Withdraw to account',
+                    style: TextStyle(
+                        color: ColorPalette.white,
+                        fontSize: AppFonts.defaultSize),
+                  ),
+                  SizedBox(width: 12),
+                  Icon(
+                    Icons.arrow_downward,
+                    color: ColorPalette.white,
+                  )
+                ]),
+              ),
+            ),
+          ),
+          const SizedBox(height: 40),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              otherFigures(
+                  'To be cleared', earningsController.pendingClearance.value),
+              otherFigures('All withdrawals',
+                  earningsController.availableForWithdrawal.value),
+              otherFigures(
+                  'Total earnings',
+                  earningsController.balance.value +
+                      earningsController.withdrawn.value),
+            ],
+          ),
+          const SizedBox(height: 40),
+          const Transactions()
+        ]));
+  }
+
+  Container otherFigures(String title, double amount) {
+    return Container(
+        height: 100,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(8),
+            border: Border.all(width: 1, color: ColorPalette.grey)),
+        child: Center(
+          child: Container(
+              margin: const EdgeInsets.only(left: 4, right: 4),
+              padding: const EdgeInsets.only(left: 4, right: 4),
+              height: 90,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(4),
+                  border: Border.all(width: 1, color: ColorPalette.grey)),
+              child: Center(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SizedBox(
+                        child: Text(
+                      title,
+                      textAlign: TextAlign.center,
+                    )),
+                    Text(formatCurrency!.format(amount),
+                        style:
+                            TextStyle(fontFamily: AppFonts.josefinSansSemiBold))
+                  ],
+                ),
+              )),
+        ));
+  }
+}
--- lib/features/wallet/presentation/widgets/transaction_item.dart
+++ lib/features/wallet/presentation/widgets/transaction_item.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:tl_consultant/app/presentation/theme/colors.dart';
 import 'package:tl_consultant/app/presentation/theme/fonts.dart';
 import 'package:tl_consultant/core/constants/constants.dart';
@@ -9,44 +10,12 @@ import 'package:tl_consultant/core/utils/extensions/date_time_extension.dart';
 part 'transactions.dart';
 
 class TransactionItem extends StatelessWidget {
-  const TransactionItem({Key? key, required this.date, required this.transactions}) : super(key: key);
-
-  final String date;
-  final List transactions;
-
-  //final amount = transaction['amount'];
-  //                         final isCredit = transaction['is_credit'];
-  //                         final status = transaction['status'];
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        SizedBox(height: 24),
-        Text(date,style: TextStyle(fontSize: 16),),
-        Container(height: 2, color: ColorPalette.grey),
-        ListView.builder(
-          padding: const EdgeInsets.only(top: 4),
-          itemCount: transactions.length,
-          shrinkWrap: true,
-          itemBuilder: (context, index)=>
-              EachTransaction(
-                  amount: transactions[index]['amount'].toString(), time: DateTime.parse(date).formattedTime, withdrawn: transactions[index]['is_credit'] ? false : true),
-        ),
-      ],
-    );
-  }
-}
-
-
-class EachTransaction extends StatelessWidget {
-  const EachTransaction({
-    Key? key,
-    required this.amount,
-    required this.time,
-    required this.withdrawn
-  }) : super(key: key);
+  const TransactionItem(
+      {Key? key,
+      required this.amount,
+      required this.time,
+      required this.withdrawn})
+      : super(key: key);
 
   final String amount;
   final String time;
@@ -54,7 +23,30 @@ class EachTransaction extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
+    return Container(
+      padding: EdgeInsets.all(12),
+      margin: EdgeInsets.only(top: 20),
+      decoration: BoxDecoration(color: ColorPalette.green[100], borderRadius: BorderRadius.circular(8)),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(withdrawn ? "Withdrawal" : "Session payment", style: TextStyle(
+                fontFamily: AppFonts.josefinSansBold)),
+          Text(
+            withdrawn ? "- $amount" : amount,
+            style: TextStyle(
+                color: withdrawn ? ColorPalette.red : ColorPalette.green[800],
+                fontFamily: AppFonts.josefinSansBold),
+          ),
+          Text(time)
+        ],
+      ),
+    );
+  }
+}
+
+/**
+ * Padding(
       padding: EdgeInsets.only(top: 8),
       child: Stack(
         children: [
@@ -96,7 +88,5 @@ class EachTransaction extends StatelessWidget {
           ),
         ],
       ),
-    );
-  }
-}
-
+    )
+ */
