3e2a5ee29726267284f6511bab227cd29de04bf9
SKPnation <ayomideseaz@gmail.com>
2025-03-12 13:12:13 -0500

    added coming soon snacbar to the growth kit selection, improved permission messages in info_plist for location and camera

--- lib/features/growth_kit/presentation/controllers/growth_kit_controller.dart
+++ lib/features/growth_kit/presentation/controllers/growth_kit_controller.dart
@@ -1,49 +1,60 @@
 import 'dart:ui';
 
 import 'package:get/get.dart';
+import 'package:tl_consultant/core/global/custom_snackbar.dart';
+import 'package:tl_consultant/core/theme/colors.dart';
 import 'package:tl_consultant/core/utils/helpers/svg_elements.dart';
 import 'package:tl_consultant/features/growth_kit/domain/entities/growth_resource.dart';
 
-class GrowthKitController extends GetxController{
+class GrowthKitController extends GetxController {
   static GrowthKitController instance = Get.find();
 
   var resources = <GrowthResource>[
     GrowthResource(
         image: SvgElements.svgSelfAssess,
         title: "Self-Assessment",
-        onTap: (){
-          print("Assessment");
-
+        onTap: () {
+          CustomSnackBar.showSnackBar(
+              context: Get.context,
+              message: "Coming soon",
+              backgroundColor: ColorPalette.blue);
         }),
-
     GrowthResource(
         image: SvgElements.svgPeerSupport,
         title: "Peer support",
-        onTap: (){
-          print("Peer support");
+        onTap: () {
+          CustomSnackBar.showSnackBar(
+              context: Get.context,
+              message: "Coming soon",
+              backgroundColor: ColorPalette.blue);
         }),
-
     GrowthResource(
-        image: SvgElements.svgCourses,
-        title: "Courses",
-        onTap: (){}),
-
+        image: SvgElements.svgCourses, title: "Courses", onTap: () {
+      CustomSnackBar.showSnackBar(
+          context: Get.context,
+          message: "Coming soon",
+          backgroundColor: ColorPalette.blue);
+    }),
     GrowthResource(
-        image: SvgElements.svgWebinars,
-        title: "Webinars",
-        onTap: (){}),
-
+        image: SvgElements.svgWebinars, title: "Webinars", onTap: () {
+      CustomSnackBar.showSnackBar(
+          context: Get.context,
+          message: "Coming soon",
+          backgroundColor: ColorPalette.blue);
+    }),
     GrowthResource(
-        image: SvgElements.svgLibrary,
-        title: "Resource library",
-        onTap: (){}),
-
+        image: SvgElements.svgLibrary, title: "Resource library", onTap: () {
+      CustomSnackBar.showSnackBar(
+          context: Get.context,
+          message: "Coming soon",
+          backgroundColor: ColorPalette.blue);
+    }),
     GrowthResource(
-        image: SvgElements.svgAnalytics,
-        title: "Analytics",
-        onTap: (){}),
+        image: SvgElements.svgAnalytics, title: "Analytics", onTap: () {
+      CustomSnackBar.showSnackBar(
+          context: Get.context,
+          message: "Coming soon",
+          backgroundColor: ColorPalette.blue);
+    }),
   ];
-
-
 }
-
