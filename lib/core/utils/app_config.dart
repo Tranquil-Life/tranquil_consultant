7074896548a1d992f279cfb840153d74ac5baaf3
SKPnation <ayomideseaz@gmail.com>
2025-10-29 08:12:18 -0500

    prevent AppConfig file from pushing

--- lib/core/utils/app_config.dart
+++ lib/core/utils/app_config.dart
@@ -1,6 +1,15 @@
 class AppConfig{
   static const appName = "Tranquil Life Pro";
+
+  //pusher
   static const pusherKey = "8148c90f95079d26d7bf";
   static const secret = "a6de5c3eae07f7117bd8";
   static const cluster = "eu";
+
+  //Paystack
+  static const paystackPublicKey = 'pk_test_2442c1c75c79a8cbd1fdd8cba558a68ea1dd8524';
+  //Google map
+  static const mapApiKey = "AIzaSyDVIeUnWdJiSLrWnI4_PaBAxYIFWd9rb7M";
+  //Agora
+  static const agoraAppId = 'a2782460e26a405cb9ffda0ae62e8038';
 }
