2e933b05e6a195805f6f65ba80fcc46198d72c03
SKPnation <ayomideseaz@gmail.com>
2025-12-17 19:18:20 -0600

    fixed bugs in 88701515e73210680ca14226ec0cc855c5c73f66 branch

--- lib/core/utils/routes/app_routes.dart
+++ lib/core/utils/routes/app_routes.dart
@@ -3,7 +3,33 @@ part of 'app_pages.dart';
 abstract class Routes {
   Routes._();
 
-  static const SPLASH_SCREEN = '/splash-screen';
+  static const SPLASH_SCREEN = _Paths.SPLASH_SCREEN;
+  static const ONBOARDING = _Paths.ONBOARDING;
+  static const SIGN_IN = _Paths.SIGN_IN;
+  static const INTRODUCE_YOURSELF = _Paths.INTRODUCE_YOURSELF;
+  static const PROFILE_PREVIEW = _Paths.PROFILE_PREVIEW;
+  static const SIGN_UP_0 = _Paths.SIGN_UP_0 ;
+  static const SIGN_UP_1 = _Paths.SIGN_UP_1 ;
+  static const SIGN_UP_2 = _Paths.SIGN_UP_2 ;
+  static const HOME_SCREEN = _Paths.HOME_SCREEN ;
+  static const DASHBOARD = _Paths.DASHBOARD ;
+  static const EDIT_SLOTS =_Paths.EDIT_SLOTS ;
+  static const QUESTIONNAIRE = _Paths.QUESTIONNAIRE ;
+  static const CHAT_SCREEN = _Paths.CHAT_SCREEN ;
+  static const WALLET = _Paths.WALLET;
+  static const NOTES_SCREEN = _Paths.NOTES_SCREEN ;
+  static const PROFILE = _Paths.PROFILE ;
+  static const EDIT_PROFILE = _Paths.EDIT_PROFILE ;
+  static const SETTINGS = _Paths.SETTINGS ;
+  static const VERIFY_RESET_ACCOUNT = _Paths.VERIFY_RESET_ACCOUNT ;
+  static const UPDATE_PASSWORD = _Paths.UPDATE_PASSWORD ;
+  static const FORGOT_PASSWORD = _Paths.FORGOT_PASSWORD ;
+}
+
+abstract class _Paths {
+  _Paths._();
+
+  static const SPLASH_SCREEN = '/splash';
   static const ONBOARDING = '/onboarding';
   static const SIGN_IN = '/sign-in';
   static const INTRODUCE_YOURSELF = '/introduce-yourself';
